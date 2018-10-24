559accdfad3365bb8823f607f1a7a09f6de90329
work_desktop <xyqinsong@163.com>
2018-10-24 16:44:58 +0800

    change project folder

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import routes from './routes'
+import { getToken } from '../common/auth'
+
+// 进度条
+// import NProgress from 'nprogress'
+// import 'nprogress/nprogress.css'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  scrollBehavior: () => ({ y: 0 }),
+  routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+  // NProgress.start()
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title || 'Sysuke\'s Blog Admin'
+})
+router.onError((err) => {
+})
+export default router
